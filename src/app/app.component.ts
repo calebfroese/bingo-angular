4c38a1f364c7d0acf7d5458f345837463ac455e8
Caleb Froese <calebfroese@Calebs-iMac.local>
2018-08-25 15:54:39 +0930

    Instantiate the appsync client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppsyncService } from './appsync.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bingo-angular';
+
+  constructor(public service: AppsyncService) {}
+
+  ngOnInit() {
+    this.service.hc().then(client => {
+      console.log(client);
+    });
+  }
 }
