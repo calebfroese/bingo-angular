2423f203162473e4768d210bdf1b4c6da9947bf8
Caleb Froese <calebfroese@Calebs-iMac.local>
2018-08-25 17:45:34 +0930

    Add subscription to players

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,26 @@ import { AppsyncService } from './appsync.service';
 
 import gql from 'graphql-tag';
 
-const join = gql`
+const joinGame = gql`
   mutation joinGame($username: String!) {
-    joinGame(username: $username)
+    joinGame(username: $username) {
+      username
+    }
+  }
+`;
+const allPlayers = gql`
+  query {
+    allPlayers {
+      username
+    }
+  }
+`;
+
+const playersChanged = gql`
+  subscription {
+    subscribeToPlayersChanged {
+      username
+    }
   }
 `;
 
@@ -15,16 +32,42 @@ const join = gql`
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'bingo-angular';
+  username = '';
+  client: any;
+  players: any[];
 
   constructor(public service: AppsyncService) {}
 
   ngOnInit() {
     this.service.hc().then(client => {
-      client.mutate({
-        mutation: join,
-        variables: { username: 'Caleb' }
+      this.client = client;
+
+      const obs = client.watchQuery({
+        query: allPlayers,
+        fetchPolicy: 'cache-and-network'
       });
+      obs.subscribe(({ data }: any) => {
+        console.log('Query fired');
+        this.players = data.allPlayers;
+      });
+
+      const subscription = obs.subscribeToMore({
+        document: playersChanged,
+        updateQuery: (prev: any, data) => ({
+          ...prev,
+          allPlayers: [
+            ...prev.allPlayers,
+            data.subscriptionData.data.subscribeToPlayersChanged
+          ]
+        })
+      });
+    });
+  }
+
+  joinGame(username: string) {
+    this.client.mutate({
+      mutation: joinGame,
+      variables: { username }
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -6,7 +7,7 @@ import { AppsyncService } from './appsync.service';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ReactiveFormsModule, FormsModule],
   providers: [AppsyncService],
   bootstrap: [AppComponent]
 })
